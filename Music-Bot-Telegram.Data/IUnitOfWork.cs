41322e44a3ccaf7b1f394879585aef0f83c63bfa
Larry <66905431+larry-oj@users.noreply.github.com>
2022-08-20 15:26:10 +0300

    fix uow interface

--- Music-Bot-Telegram.Data/IUnitOfWork.cs
+++ Music-Bot-Telegram.Data/IUnitOfWork.cs
@@ -1,7 +1,13 @@
-﻿namespace Music_Bot_Telegram.Data;
+﻿using Music_Bot_Telegram.Data.Models;
+
+namespace Music_Bot_Telegram.Data;
 
 public interface IUnitOfWork : IDisposable
 {
+    Repo<User> Users { get; }
+    Repo<Models.Action> Actions { get; }
+    Repo<ActionType> ActionTypes { get; }
+    
     void Save();
     Task SaveAsync();
 }
