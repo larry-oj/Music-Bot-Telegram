3633ffc53e0a51856ab2564a2b20f2c4383b4cb1
Larry <66905431+larry-oj@users.noreply.github.com>
2022-08-20 15:02:20 +0300

    implement db context

---
+++ Music-Bot-Telegram.Data/BotDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Music_Bot_Telegram.Data.Models;
+
+namespace Music_Bot_Telegram.Data;
+
+public class BotDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Models.Action> Actions { get; set; }
+    public DbSet<ActionType> ActionTypes { get; set; }
+
+    public BotDbContext(DbContextOptions<BotDbContext> options)
+        : base(options)
+    {
+        // ...
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // ...
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ActionType>()
+            .HasIndex(u => u.Name)
+            .IsUnique();
+    }
+}
