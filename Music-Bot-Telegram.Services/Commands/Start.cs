4a123c085be2ae203841f75dd2a7bb984259b1f3
Larry <66905431+larry-oj@users.noreply.github.com>
2022-08-31 17:03:45 +0300

    add start command

---
+++ Music-Bot-Telegram.Services/Commands/Start.cs
@@ -0,0 +1,25 @@
+﻿using Music_Bot_Telegram.Data;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using User = Music_Bot_Telegram.Data.Models.User;
+
+namespace Music_Bot_Telegram.Services.Commands;
+
+public class Start : ICommand
+{
+    public string Name 
+        => "start";
+    
+    public string Description 
+        => "Bot introduction";
+
+    public bool IsAdmin 
+        => false;
+    
+    public async Task ExecuteAsync(ITelegramBotClient botClient, Message message, User user)
+    {
+        await botClient.SendTextMessageAsync(
+            chatId: message.Chat.Id,
+            text: "Greetings!\n I am a musical bird, that is capable of:\n 1 - Recognizing music from audio or video you send me!\n 2 - Create and send you .mp3 files of your favourite songs!\n 3 - Search music across YouTube and Spotify!\n \n Use /help to see the list of all commands!\n \n Creator - https://larryoj.tk\n Project GitHub - https://github.com/larry-oj/Music-Bot-Telegram");
+    }
+}
